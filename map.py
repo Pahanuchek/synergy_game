59566486f0b14a47599b6de1ae1ef63e451d95c2
Pavel Novozhilov <p.novozhilov24@mail.ru>
2023-02-27 22:36:16 +0300

    map creation

--- map.py
+++ map.py
@@ -1,1 +1,31 @@
-#🌲🌊🔥☁️🌩🚁
+CELL_TYPES = '🏾🌲🌊🏩🏬'
+
+class Map:
+    #def generate_rivers(self):
+
+    #def generate_forests(self):
+
+    def print_map(self):
+        print('⬛' * (self.w + 2))
+        for row in self.cells:
+            print('⬛', end='')
+            for cell in row:
+                if (cell >= 0 or cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell], end='')
+            print('⬛', end='')
+            print()
+        print('⬛' * (self.w + 2))
+
+    def check_bounds(self, x, y):
+        if (x < 0 or y < 0 or x >= self.w or x >= self.h):
+            return False
+        else:
+            True
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells = [[0 for i in range(w)] for j in range(h)]
+
+tmp = Map(20, 10)
+tmp.print_map()
